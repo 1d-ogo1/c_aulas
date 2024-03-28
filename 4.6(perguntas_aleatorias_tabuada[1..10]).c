0e25784bf5f09b23e40a10cd508719e34f229787
1d-ogo1 <116488327+1d-ogo1@users.noreply.github.com>
2024-03-28 12:07:27 +0000

    Create 4.6(perguntas_aleatorias_tabuada[1..10]).c

---
+++ 4.6(perguntas_aleatorias_tabuada[1..10]).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define PERGUNTAS 10
+
+int main()
+{
+    int valor_1, valor_2, input, certas = 0;
+
+    for (int i = 0; i < PERGUNTAS; i++)
+    {
+        //Atribuição dos valores a serem multiplicados
+        srand(time(NULL));
+        valor_1 = (rand() % 8) + 1;
+        srand(time(NULL)*2);
+        valor_2 = (rand() % 8) + 1;
+        printf("Quanto é %dx%d?: ", valor_1, valor_2);
+        scanf("%d", &input);
+        if (input == valor_1 * valor_2) {
+            printf("Certo!\n");
+            certas ++;
+        }
+        else printf("Errado! O resultado é %d\n", (valor_1 * valor_2));
+    }
+    printf("Acertou %d de %d\n", certas, PERGUNTAS);
+    return 0;
+}
