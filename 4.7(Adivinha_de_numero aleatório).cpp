44b4840c3c282acabae15c4bec3db580ff561dfd
1d-ogo1 <116488327+1d-ogo1@users.noreply.github.com>
2024-03-31 16:18:58 +0100

    Create 4.7(Adivinha_de_numero aleatório).cpp

---
+++ 4.7(Adivinha_de_numero aleatório).cpp
@@ -0,0 +1,22 @@
+#
+
+int main()
+{
+    int n_secreto, resp, tentativas = 0;
+    srand(time(NULL));
+    n_secreto = (rand() % 1000) + 1;
+    printf("Um jogo para adivinhar o numero secreto entre 1 e 1000.\n"
+           "Nas menores tentativas possiveis!!\n");
+
+    do {
+        printf("Escolhe um numero :: ");
+        scanf("%d", &resp);
+        tentativas++;
+        if (resp < n_secreto) printf("Demasiado baixo!!\n");
+        else if(resp > n_secreto) printf("Demasiado alto!!\n");
+    }while(resp != n_secreto);
+
+    printf("Acertou em %d tentativas!!\n", tentativas);
+    return 0;
+
+}
